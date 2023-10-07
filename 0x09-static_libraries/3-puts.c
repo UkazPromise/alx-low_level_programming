11bc1364083c6b7325cfffffe1481a8775642ab4
UkazPromise <ukazpromise@gmail.com>
2023-10-06 20:42:25 -0700

    create_static_lib.sh

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,14 @@
 #include "main.h"
-#include "stdio.h"
-void _puts(char *str)
+#include <stdio.h>
+
+void _puts(char *s)
 {
-
+    (void)s;
+    /* My code for the _puts function goes here */
 }
-#include<stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}

2f34657a2b118f22415538edea41f62894159b61
UkazPromise <ukazpromise@gmail.com>
2023-10-09 19:55:06 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,15 +10,23 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int i = 0, l = 0;
 	char *p;
-	size_t i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
+
+	l = nmemb * size;
+	p = malloc(l);
+
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		p[1] = 0;
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+
 	return (p);
 }

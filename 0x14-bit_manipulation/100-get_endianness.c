98b213f2d4aad54d4b3d79d517dd549d3acdd8de
UkazPromise <ukazpromise@gmail.com>
2023-10-27 16:52:30 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+    unsigned int test = 1;
+    unsigned char *byte_ptr = (unsigned char *)&test;
+
+    return (byte_ptr[0] == 1);
+}

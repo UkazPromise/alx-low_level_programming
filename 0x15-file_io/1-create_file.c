15faed60f3aaa1c1833687c65afca28d36d768fc
UkazPromise <ukazpromise@gmail.com>
2023-10-30 17:36:43 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - Create a file with specific permissions and write text to it.
+ * @filename: The name of the file to create.
+ * @text_content: The text content to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int file_descriptor, write_result;
+    mode_t file_permissions = S_IRUSR | S_IWUSR;
+
+    if (filename == NULL)
+        return (-1);
+
+    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
+    if (file_descriptor == -1)
+        return (-1);
+
+    if (text_content != NULL)
+    {
+        write_result = write(file_descriptor, text_content, strlen(text_content));
+        if (write_result == -1)
+        {
+            close(file_descriptor);
+            return (-1);
+        }
+    }
+
+    close(file_descriptor);
+    return (1);
+}
